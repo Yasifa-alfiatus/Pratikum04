cfaceb1a7972ab324b94d4557f8a8c8daf531d12
Yasifa-alfiatus <yasifa.alfiatus13@smk.belajar.id>
2026-09-16 09:51:22 +0700

    Mengubah if-else menjadi ternary operator

--- PemilihanBilangan30.java
+++ PemilihanBilangan30.java
@@ -2,20 +2,15 @@ import java.util.Scanner;
 
 public class PemilihanBilangan30 {
     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-         System.out.println("Masukan sebuah angka: ");
-         int angka = sc.nextInt();
+        System.out.print("Masukan sebuah angka: ");
+        int angka = sc.nextInt();
 
-         if (angka % 2 == 0)
-         {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-         }
-         else
-         {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-         }
-
+        System.out.println(
+            (angka % 2 == 0)
+            ? "Angka " + angka + " termasuk bilangan genap"
+            : "Angka " + angka + " termasuk bilangan ganjil"
+        );
     }
 }
- 
